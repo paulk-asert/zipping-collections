89d270eab3d55ae176e648b7a127ce45deb92ee6
Paul King <paulk@asert.com.au>
2022-11-17 21:45:05 +1000

    initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zipping collections with Groovy
+
+![Fall Emoji](./docs/images/fall.png)
+
+A Groovy solution inspired by:
+https://donraab.medium.com/make-or-append-me-a-string-c654f247373a
+
+Related blog post:
+https://blogs.apache.org/groovy/entry/zipping-collections-with-groovy
---
+++ src/main/groovy/FallFusionGinq.groovy
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fall1 = GQ { from one in ['🎃', '🍎', '🍁', '🌽'] select _rn, one }
+var fall2 = GQ { from two in ['🥧', '🥧', '🍂', '🍞'] select _rn, two }
+var zipped = GQL {
+    from one in fall1
+    join two in fall2 on one._rn == two._rn
+    select "$one.one$two.two"
+}.join(' + ')
+var fusion = "[$zipped] = Fall Fusion"
+assert fusion == '[🎃🥧 + 🍎🥧 + 🍁🍂 + 🌽🍞] = Fall Fusion'
